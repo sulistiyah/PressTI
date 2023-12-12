bbed9590c3994e679ff4a1d432f5b4bdfad589e5
sulistiyah <sulistyhm.03@gmail.com>
2023-12-12 20:57:28 +0700

    PressTI

--- server-api/app/src/server.js
+++ server-api/app/src/server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://34.192.213.125:3000');
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
